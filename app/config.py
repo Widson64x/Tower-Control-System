f6a51c0a4da5d8b670647a80b9c2b7d42e495197
Widson Rodrigues <widsonrodrigues3@gmail.com>
2025-06-03 12:02:43 -0300

    Atualiza Config para apontar para o banco no Render

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:luft%40123@localhost:5432/equipe_db"
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://towerdb_user:"
+        "afv3K6q3bN24gO8G0ohwspbBEnodHtSa"
+        "@dpg-d0vg35vdiees73csea30-a.oregon-postgres.render.com"
+        ":5432/equipe_db?sslmode=require"
+    )
     SECRET_KEY = "uma_senha_supersecreta"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
